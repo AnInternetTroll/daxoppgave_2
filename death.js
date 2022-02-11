e4a0f71efbee49959505a7d993c58cb3fdf5aeb0
Luca Matei Pintilie <lucafulger@gmail.com>
2022-02-11 17:30:24 +0100

    Better death time formatting, but it still needs work!

--- death.js
+++ death.js
@@ -3,15 +3,31 @@ const timeLeft = new URL(location.href).searchParams.get("time-left");
 // Precision is a myth
 const timeLeftDate = new Date();
 timeLeftDate.setFullYear(timeLeftDate.getFullYear() + parseInt(timeLeft));
+const { format } = new Intl.DateTimeFormat(
+	navigator.language, {
+		day: "2-digit",
+		month: "long",
+		year: "numeric",
+	}
+);
 
 setInterval(() => {
 	const now = new Date();
-	console.log(now, timeLeftDate);
-	timeLeftEl.textContent = `year ${
-		timeLeftDate.getFullYear() - now.getFullYear()
-	} month ${timeLeftDate.getMonth() - now.getMonth()} day ${
-		timeLeftDate.getDay() - now.getDay()
-	} ${timeLeftDate.getHours() - now.getHours()}:${
-		timeLeftDate.getMinutes() - now.getMinutes()
-	}:${timeLeftDate.getSeconds() - now.getSeconds()}`;
+	const secondsToDeath = milsToSeconds(Date.now() - timeLeftDate.getMilliseconds());
+	const minutesToDeath = secondsToMinutes(secondsToDeath);
+	const hoursToDeath = minToHours(minutesToDeath);
+	const daysToDeath = hoursToDays(hoursToDays);
+	console.log('we do a little trolling');
+
+	timeLeftEl.textContent = `
+		Death date: ${format(timeLeftDate)}
+		${daysToDeath}d ${hoursToDeath}h ${minutesToDeath}m ${secondsToDeath}s
+		`;
 }, 1000);
+
+const milsToSeconds = (m) => m / 1000;
+const secondsToMinutes = (s) => s / 60;
+const minToHours = (min) => min / 60;
+const hoursToDays = (h) => h / 24;
+const daysToYears = (d) => d / 365;
+
